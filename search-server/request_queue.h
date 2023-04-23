78d6f39dda0e89695db52e9d290ba186321a28c9
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-04-23 16:12:13 +0300

    Create request_queue.h

---
+++ search-server/request_queue.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "search_server.h"
+#include <vector>
+#include <deque>
+
+class RequestQueue {
+public:
+    explicit RequestQueue(const SearchServer& search_server);
+    // сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
+    template <typename DocumentPredicate>
+    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate);
+    vector<Document> AddFindRequest(const string& raw_query, DocumentStatus status);
+    vector<Document> AddFindRequest(const string& raw_query);
+    int GetNoResultRequests() const;
+private:
+    struct QueryResult {
+        vector<Document> documents_;
+    };
+    deque<QueryResult> requests_;
+    const static int min_in_day_ = 1440;
+    int request_num_ = 0;
+    const SearchServer *server_;
+};
+
+template <typename DocumentPredicate>
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query, DocumentPredicate document_predicate) {
+        ++request_num_;
+        if(request_num_ > min_in_day_)
+        {
+        	requests_.pop_front();
+        	--request_num_;
+        }
+        requests_.push_back({server_->FindTopDocuments(raw_query, document_predicate)});
+        return server_->FindTopDocuments(raw_query, document_predicate);
+    }
