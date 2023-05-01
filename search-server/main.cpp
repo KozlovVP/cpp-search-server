348d801284f5ac2ec000f244e728006a9ca2a684
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-05-01 11:14:34 +0300

    Update main.cpp

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -7,19 +7,4 @@
 #include "search_server.h"
 #include "string_processing.h"
 
-template <typename Iterator>
-ostream& operator<<(ostream &os, IteratorRange<Iterator> page) 
-{
-    for (auto i = page.begin(); i < page.end(); i++)
-    {
-        cout << *i;
-    }
-    return os;
-}
-
-ostream& operator<<(ostream &os, const Document& doc)
-{
-    return os << "{ document_id = " << doc.id
-               << ", relevance = "   << doc.relevance
-               << ", rating = "      << doc.rating << " }";
-}
+using namespace std; // для тренажера
