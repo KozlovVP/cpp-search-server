9a9733fb727949c464345f7a496ffdce9951ca2a
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-05-01 11:17:42 +0300

    Update read_input_functions.cpp

--- search-server/read_input_functions.cpp
+++ search-server/read_input_functions.cpp
@@ -2,15 +2,15 @@
 
 #include "read_input_functions.h"
 
-string ReadLine() {
-    string s;
-    getline(cin, s);
+std::string ReadLine() {
+    std::string s;
+    std::getline(std::cin, s);
     return s;
 }
 
 int ReadLineWithNumber() {
     int result;
-    cin >> result;
+    std::cin >> result;
     ReadLine();
     return result;
 }
