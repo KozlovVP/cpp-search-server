f8f3b9d6a250beb8983c1bfea5ff8935008d01ae
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-05-02 18:36:32 +0300

    Update search_server.h

--- search-server/search_server.h
+++ search-server/search_server.h
@@ -32,9 +32,16 @@ public:
 
     int GetDocumentCount() const;
 
-    int GetDocumentId(int index) const;
+    //int GetDocumentId(int index) const;
 
     std::tuple<std::vector<std::string>, DocumentStatus> MatchDocument(const std::string& raw_query, int document_id) const;
+    
+    std::vector<int>::const_iterator begin() const;
+    std::vector<int>::const_iterator end() const;
+    
+    const std::map<std::string, double>& GetWordFrequencies(int document_id) const;
+    
+    void RemoveDocument(int document_id);
 
 private:
     struct DocumentData {
@@ -45,6 +52,7 @@ private:
     std::map<std::string, std::map<int, double>> word_to_document_freqs_;
     std::map<int, DocumentData> documents_;
     std::vector<int> document_ids_;
+    std::map<int, std::map<std::string, double>> document_id_to_words_freq_;
 
     bool IsStopWord(const std::string& word) const;
 
