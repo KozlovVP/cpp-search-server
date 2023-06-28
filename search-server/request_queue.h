f2a49827062c5fce06d75fc9918d92f10519db64
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-06-28 23:37:14 +0300

    Update request_queue.h

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -5,34 +5,33 @@
 
 class RequestQueue {
 public:
-    explicit RequestQueue(const SearchServer& search_server);
-    // сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
+    explicit RequestQueue(const SearchServer& search_server): server_(search_server) {
+    }
     template <typename DocumentPredicate>
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
     std::vector<Document> AddFindRequest(const std::string& raw_query);
     int GetNoResultRequests() const;
+
 private:
     struct QueryResult {
-        //vector<Document> documents_;
-        QueryResult(int size) 
-            :docs_count(size) {}
-        int docs_count;
+        std::vector<Document> documents_;
     };
     std::deque<QueryResult> requests_;
     const static int min_in_day_ = 1440;
     int request_num_ = 0;
-    const SearchServer *server_;
+    const SearchServer& server_;
 };
 
 template <typename DocumentPredicate>
 std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        ++request_num_;
-        if(request_num_ > min_in_day_)
-        {
-        	requests_.pop_front();
-        	--request_num_;
-        }
-        requests_.push_back(QueryResult(server_->FindTopDocuments(raw_query, document_predicate).size()));
-        return server_->FindTopDocuments(raw_query, document_predicate);
+	using namespace std;
+    ++RequestQueue::request_num_;
+    if(RequestQueue::request_num_ > RequestQueue::min_in_day_)
+    {
+    	RequestQueue::requests_.pop_front();
+    	--RequestQueue::request_num_;
     }
+    RequestQueue::requests_.push_back({RequestQueue::server_.FindTopDocuments(raw_query, document_predicate)});
+    return RequestQueue::server_.FindTopDocuments(raw_query, document_predicate);
+}
