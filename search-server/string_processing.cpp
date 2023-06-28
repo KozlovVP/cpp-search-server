7edbad61c400ce3378014a82e99c7fcdd77749a3
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-06-28 23:38:18 +0300

    Update string_processing.cpp

--- search-server/string_processing.cpp
+++ search-server/string_processing.cpp
@@ -1,9 +1,9 @@
 #include "string_processing.h"
 
-
-std::vector<std::string> SplitIntoWords(const std::string& text) {
-    std::vector<std::string> words;
-    std::string word;
+/*std::vector<std::string> SplitIntoWords(const std::string& text) {
+	using namespace std;
+    vector<string> words;
+    string word;
     for (const char c : text) {
         if (c == ' ') {
             if (!word.empty()) {
@@ -18,5 +18,32 @@ std::vector<std::string> SplitIntoWords(const std::string& text) {
         words.push_back(word);
     }
 
+    return words;
+}*/
+
+std::vector<std::string_view> SplitIntoWords(const std::string_view& text)
+{
+	using namespace std;
+    vector<string_view> words;
+    auto next_it = text.begin();
+    for(auto it = text.begin(); it!= text.end(); ++it)
+    {
+    	if(*it == ' ')
+    	{
+    		if(next_it != it)
+    		{
+    			words.push_back(string_view(next_it, (it - next_it)));
+    			next_it = it+1;
+    		}
+    		else
+    		{
+    			++next_it;
+    		}
+    	}
+    }
+    if(next_it != text.end())
+    {
+    	words.push_back(string_view(next_it, (text.end() - next_it)));
+    }
     return words;
 }
