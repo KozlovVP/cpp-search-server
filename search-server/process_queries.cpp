7c5635f55449fd409e60a68dcec300caac0dcd2b
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-06-30 09:53:27 +0300

    Update process_queries.cpp

--- search-server/process_queries.cpp
+++ search-server/process_queries.cpp
@@ -3,21 +3,14 @@
 std::vector<std::vector<Document>> ProcessQueries(const SearchServer& search_server, const std::vector<std::string>& queries)
 {
 	std::vector<std::vector<Document>> documents_lists(queries.size());
-	transform(execution::par, queries.begin(), queries.end(), documents_lists.begin(), [&search_server](const std::string& local_query){return search_server.FindTopDocuments(local_query);});
+	transform(std::execution::par, queries.begin(), queries.end(), documents_lists.begin(), [&search_server](const std::string& local_query){return search_server.FindTopDocuments(local_query);});
 	return documents_lists;
 }
 
 std::list<Document> ProcessQueriesJoined(const SearchServer& search_server, const std::vector<std::string>& queries)
 {
-	/*std::list<Document> documents_list;
-	for(const auto& query : queries)
-	{
-		for(const auto& document : search_server.FindTopDocuments(query))
-		{
-			documents_list.push_back(document);
-		}
-	}*/
-	vector<vector<Document>> tmp_doc_vector = ProcessQueries(search_server, queries);
+
+	std::vector<std::vector<Document>> tmp_doc_vector = ProcessQueries(search_server, queries);
 	std::list<Document> documents_list;
 	for(auto it = tmp_doc_vector.begin(); it!=tmp_doc_vector.end(); ++it)
 	{
